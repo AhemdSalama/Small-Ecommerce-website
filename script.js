0848324550c9b82f79301e7b3b95304d08351188
Ahmed Salama <ahmedsalamafcis@gmail.com>
2026-02-09 22:28:03 +0200

    Was able to create the global cartItem object, user can add the item to cart and view the details. Next is to display the items on the shopping cart

--- script.js
+++ script.js
@@ -3,14 +3,18 @@
 const listCardGrid = document.getElementById('listCardGrid');
 const loadPrevious = document.getElementById('loadPrevious');
 const loadNext = document.getElementById('loadNext');
-let totalProducts 
+const activePaginatedBtn = document.getElementById('activePaginatedBtn');
+const cartItems = []; // global cart items
+
+let totalProducts;
 let page = 1;
 const productsPerPage = 6;
 
-let lenght = 0;
+
 const loadProducts = async ()=>{
     const start = (page -1) * productsPerPage;
     const end = start + productsPerPage;
+    activePaginatedBtn.innerHTML = page
     // const paginateURL = `https://fakestoreapi.com/products?limit=${productsPerPage}&skip=${skip}`;
     const allProductsURL= "https://fakestoreapi.com/products";
 
@@ -65,6 +69,59 @@ loadNext.addEventListener('click',()=>{
 loadProducts();
 
 //  viewDetails
+async function viewDetails(productId)
+{   
+    const productModal = document.getElementById('productModal');
+    const modalBody = document.getElementById('modalBody');
 
+    const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    const product = await res.json();
+    
+    productModal.classList.add('active');
+    modalBody.innerHTML = 
+    `
+        <div class="modal-detail-container">
+            <img src="${product.image}" alt="${product.title}">
+            <div>
+                <span class="category-badge">${product.category}</span>
+                <h2>${product.title}</h2>
+                <p><strong>Rating:</strong> ${product.rating.rate} / 5</p>
+                <p>${product.description}</p>
+                <h3 class="price">$${product.price}</h3>
+                <button class="btn-add" onclick="addToCart(${product.id})">Add to Cart</button>
+            </div>
+        </div>
+    `;
 
+}
+// closeModal
+function closeModal(){
+    const productModal = document.getElementById('productModal');
+    const modalBody = document.getElementById('modalBody');
+
+    productModal.classList.remove('active');
+    modalBody.innerHTML = "";
+}
 // addToCart
+async function addToCart(productId){
+    const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    const prod = await res.json();
+    const prodObject = {
+        item: prod,
+        quantity: 1,
+    }
+    
+    // check if the item added before
+    const existingItem = cartItems.find(item => item.item.id === prodObject.item.id);
+    if(existingItem)
+    {
+        existingItem.quantity +=1;
+    }
+    else 
+    {
+        cartItems.push(prodObject);
+    }
+    console.log(cartItems);
+    // check if the product already in cart then increase the quantity, price etc..
+
+}
